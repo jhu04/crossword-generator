e59223b921e2e236c91759746ea9659788fc8ace
Preston Fu <prestonfu@berkeley.edu>
2022-12-23 13:48:44 -0800

    patched prev

--- src/constants/scopes.jsx
+++
@@ -1,5 +0,0 @@
-export const INCOMPLETE = 'Incomplete';
-export const SQUARE = 'Square';
-export const WORD = 'Word';
-export const PUZZLE = 'Puzzle';
-export const PUZZLE_AND_TIMER = 'Puzzle & Timer';
