73a22bdb5e30c8e9b79ba86c02d3ce50b47a8ede
Preston Fu <prestonfu@berkeley.edu>
2022-12-24 00:01:38 -0800

    updated frontend

--- client/src/pages/Loading/Loading.jsx
+++ client/src/pages/Loading/Loading.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import TypeWriter from 'typewriter-effect';
+import css from './Loading.scss';
 
 export default function Loading() {
-    return (
-        <div>
-            Loading...
-        </div>
-    )
+  return (
+    <div className={css.wrapper}>
+      <h1>
+        <TypeWriter
+          options={{ 
+            strings: ['Loading...'],
+            autoStart: true,
+            loop: true
+          }}
+        />
+      </h1>
+    </div>
+  )
 }
--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import css from './NotFound.scss';
 
 export default function NotFound() {
   return (
-    <div>
-      Not Found
+    <div className={css.bgText} txt='404'>
+      <div>
+        <h1>
+          Oops!
+        </h1>
+        <h2>
+          We couldn't find the page you were looking for.
+        </h2>
+      </div>
     </div>
   )
 }
