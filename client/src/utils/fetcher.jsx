8c90404e25c0299c634b47db601b7ae4a202b21e
Preston Fu <prestonfu@berkeley.edu>
2022-12-27 23:20:01 -0800

    forgot to update more manual env

--- client/src/utils/fetcher.jsx
+++ client/src/utils/fetcher.jsx
@@ -5,8 +5,8 @@ export const STATUS_404 = 'STATUS_404';
 export function puzzleFetcher(puzzleId, headers = {}) {
   const SERVER_URL =
     (process.env.NODE_ENV === 'production')
-      ? process.env.SERVER_URL_PROD
-      : process.env.SERVER_URL_DEV;
+      ? 'https://crossify-server.vercel.app'
+      : 'http://localhost:5000'; // TODO: fix env variables in deployment
 
   return Axios.get(`${SERVER_URL}/api/id/${puzzleId}`)
     .then((res) => { return res.data; })
